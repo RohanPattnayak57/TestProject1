91f1a77d50f1bc983a15569d83830b9f7dfb5568
RohanPattnayak57 <rohanpattnayak07@gmail.com>
2020-12-29 00:05:47 +0530

    assignment2

--- TestProject1/src/Assignment1.java
+++ TestProject1/src/Assignment1.java
@@ -2,25 +2,25 @@ package one.TestProject1.src;
 import java.util.Scanner;
 
 
-public class Assignment1 {
+public class Assignment1{
 	     
-	        public static void main(String[] args) {
-	            Scanner scan=new Scanner(System.in);
-	            System.out.println("enter the number:");
-	            int a=scan.nextInt();
-	            String print="";
-	            if(a%2==1){
-	              print = "weird";
-	            }
-	            else {
-	                if (a >= 2 && a <= 5) {
-	                    print = "Not weird";
-	                } else if (a >= 6 && a <= 20) {
-	                    print = "weird";
-	                } else {
-	                    print = "Not weird";
-	                }
-	            System.out.println(print);   
-	        }
-	        }
+	public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int a=scan.nextInt();
+        String print="";
+        if(a%2==1){
+          print = "weird";
+        }
+        else {
+            if (a >= 2 && a <= 5) {
+                print = "Not weird";
+            } else if (a >= 6 && a <= 20) {
+                print = "weird";
+            } else {
+                print = "Not weird";
+            }
+        System.out.println(print);   
+    }
+    }
 }
