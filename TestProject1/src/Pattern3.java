368297723928bf56453264321349bea08bffc9ad
RohanPattnayak57 <rohanpattnayak07@gmail.com>
2020-12-29 12:09:10 +0530

    reversed of pattern 2

--- TestProject1/src/Pattern3.java
+++ TestProject1/src/Pattern3.java
@@ -3,16 +3,14 @@ package one.TestProject1.src;
 public class Pattern3 {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=i;j++) {
-				System.out.print(" ");
-			}
-			for(int k=5;k>=i;k--) {
-				System.out.print("*");
-			}
-			System.out.println();
+		for (int i=1;i<=5;i++) {
+			 for (int j=1;j<=i;j++) {
+				 System.out.print(" ");
+			 }
+			 for (int k=5;k>=i;k--) {
+				 System.out.print("*");
+			 }
+				 System.out.println();
 		}
-
+		}
 	}
-
-}
