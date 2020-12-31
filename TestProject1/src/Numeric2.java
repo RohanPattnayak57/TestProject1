15e4e272d20f1f8d6528acf5c85a16ce0eb67866
RohanPattnayak57 <rohanpattnayak07@gmail.com>
2020-12-31 15:59:04 +0530

    numeric pattern 2 iejosri

---
+++ TestProject1/src/Numeric2.java
@@ -0,0 +1,16 @@
+package one.TestProject1.src;
+
+public class Numeric2 {
+	public static void main(String args[]) {
+		int count=0;
+		for (int i=1;i<=4;i++)
+        {
+	for (int j=1;j<=i;j++)
+	 {
+		count=count+1;
+		 System.out.print(count);
+	 }
+		 System.out.println();
+}
+	}
+}
