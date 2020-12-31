9dcd812feebad6a9259144d16f0c675fc83b7437
RohanPattnayak57 <rohanpattnayak07@gmail.com>
2020-12-31 16:00:56 +0530

    numeric pattern 2

--- TestProject1/src/Numeric2.java
+++ TestProject1/src/Numeric2.java
@@ -5,9 +5,9 @@ public class Numeric2 {
 		int count=0;
 		for (int i=1;i<=4;i++)
         {
-	for (int j=1;j<=i;j++)
+	    for (int j=1;j<=i;j++)
 	 {
-		count=count+1;
+		  count=count+1;
 		 System.out.print(count);
 	 }
 		 System.out.println();
